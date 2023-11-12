731a12c19ace15d4795eac565497435eb89df22f
fabioherberth <fabioherberth@hotmail.com>
2023-11-12 16:32:18 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -29,7 +29,8 @@ if(localStorage.getItem("started")) {
 btnStart.addEventListener("click", () => {
 
     iptNameOfTheWinner.value = "Buscando...";
-    const url = "http://church-api.ddns.net:8090/draw/event/1";
+    //const url = "http://church-api.ddns.net:8090/draw/event/1";
+    const url = "church-api.ddns.net:8090/draw/event/1";
 
     let request = new Request(url, {
         method: 'GET',
